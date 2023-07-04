b1e1d3288094d1c0dc0dffbbac312b435eb5006b
Marc Cocchio <marccocchio@gmail.com>
2023-07-04 12:51:36 +0900

    use Twig template

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response; //import the Response class
 use Symfony\Component\Routing\Annotation\Route; //import the Route annotation class
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; //import the AbstractController class
 
-class LuckyController
+class LuckyController extends AbstractController //in order to render a template
 {
     #[Route('/lucky/number')] //add route directly above the controller
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>（1~100）ラッキーナンバーは'.$number.'</body></html>'
-        );
+        //return new Response(
+        //    '<html><body>（1~100）ラッキーナンバーは'.$number.'</body></html>'
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number //pass a 'number' variable to be used in Twig
+        ]);
     }
 }
